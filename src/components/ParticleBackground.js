b5a6003afe73bb98af52ff63efec353dccbb6c49
vignesh <vignesh.m@cumulations.com>
2021-02-04 19:24:49 +0530

    fix: import issue in ParticleBackground.js

--- src/components/ParticleBackground.js
+++ src/components/ParticleBackground.js
@@ -2,7 +2,7 @@ import Particles from "react-particles-js";
 import React from "react";
 import '../css/HoverAnimation.css'
 
-export default ParticleBackground = () => {
+export default function ParticleBackground() {
   return (
     <Particles className='body-particles'
       params={{
