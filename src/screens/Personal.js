8d462956467bcd453db25f7ba17f9399f5401db3
vignesh <vignesh.m@cumulations.com>
2021-07-04 17:11:45 +0530

    personalJS changed

--- src/screens/Personal.js
+++ src/screens/Personal.js
@@ -1,35 +1,35 @@
-let interestList = [
-  "Minimalism",
-  "Atheism",
-  "Periyaar",
-  "Ambedkar",
-  "Nolan Movies",
-  "U1 Songs",
-  "Solo Bike Rides",
-  "Mech KB",
-  "Coding",
-  "Sci-Fi Movies",
-  "Vijay Antony",
-  "Torantino",
-  "Long Car Rides",
-  "Ilayaraja",
-  "Paulo Coelho",
-  "Communism",
-  "Cs:GO",
-  "Ginger Tea",
-  "Slow living",
-  "Naruto",
-  "Beach",
-  "Train Travel",
-  "Bomb Squad",
-  "Desk Setup",
-  "Solid Tee",
-  "Santhosh Narayanan",
-  "Pradeep Kumar",
-  "MKBHD",
-  "Mysskin",
-  "Time Travel",
-];
+// let interestList = [
+//   "Minimalism",
+//   "Atheism",
+//   "Periyaar",
+//   "Ambedkar",
+//   "Nolan Movies",
+//   "U1 Songs",
+//   "Solo Bike Rides",
+//   "Mech KB",
+//   "Coding",
+//   "Sci-Fi Movies",
+//   "Vijay Antony",
+//   "Torantino",
+//   "Long Car Rides",
+//   "Ilayaraja",
+//   "Paulo Coelho",
+//   "Communism",
+//   "Cs:GO",
+//   "Ginger Tea",
+//   "Slow living",
+//   "Naruto",
+//   "Beach",
+//   "Train Travel",
+//   "Bomb Squad",
+//   "Desk Setup",
+//   "Solid Tee",
+//   "Santhosh Narayanan",
+//   "Pradeep Kumar",
+//   "MKBHD",
+//   "Mysskin",
+//   "Time Travel",
+// ];
 
 let shortStory =
   "A full-time Android Dev based in India, who also runs a YouTube Channel and a blog, where I post and write tech tutorials. (Yeah! Nerd.)\n\nSelectively social, constant learner, cis-het, rarely creative, occasionally good lookin and an abnormal human.\n\nI am that guy, who cuts your call and texts you back, I'll call you later, but never does.";
@@ -69,29 +69,29 @@ function IntroSection() {
   );
 }
 
-function shortStorySection() {
-  return (
-    <div>
-      <h1 className="subHeadingFiraGreen">🤘 HI, I AM VIGNESH MARIMUTHU</h1>
-      <div className="space"> </div>
-      <h1 className="superTitleWhite" data-text="Vignesh Marimuthu">
-        Minimalist | Stoic | Software Dev | Atheist | Human
-      </h1>
-    </div>
-  );
-}
+// function shortStorySection() {
+//   return (
+//     <div>
+//       <h1 className="subHeadingFiraGreen">🤘 HI, I AM VIGNESH MARIMUTHU</h1>
+//       <div className="space"> </div>
+//       <h1 className="superTitleWhite" data-text="Vignesh Marimuthu">
+//         Minimalist | Stoic | Software Dev | Atheist | Human
+//       </h1>
+//     </div>
+//   );
+// }
 
-function LikesSection() {
-  return (
-    <div className="likesCenter">
-      <div className="subHeadingEpilogueWhite padding25">
-        If you could vibe to a lot of the below, we can be great friends 🙌
-      </div>
-      <div className="likesContainer">
-        {interestList.map((item, index) => {
-          return <a class="interests buttonText">{item}</a>;
-        })}
-      </div>
-    </div>
-  );
-}
+// function LikesSection() {
+//   return (
+//     <div className="likesCenter">
+//       <div className="subHeadingEpilogueWhite padding25">
+//         If you could vibe to a lot of the below, we can be great friends 🙌
+//       </div>
+//       <div className="likesContainer">
+//         {interestList.map((item, index) => {
+//           return <a class="interests buttonText">{item}</a>;
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
