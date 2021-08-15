d349d1ef92ba260034a36331add086cb9a505f4a
vignesh <vignesh.m@cumulations.com>
2021-08-15 12:29:49 +0530

    updated to https

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: `http://vigneshmarimuthu.herokuapp.com/graphql`,
+  uri: `https://vigneshmarimuthu.herokuapp.com/graphql`,
   cache: new InMemoryCache(),
 });
 
