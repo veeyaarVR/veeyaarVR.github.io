bd9f95a7eefb8d527232dd28ea27562066cb359a
vignesh <vignesh.m@cumulations.com>
2021-08-30 21:40:21 +0530

    modified build folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: `${process.env.REACT_APP_BACKEND_URL}`,
