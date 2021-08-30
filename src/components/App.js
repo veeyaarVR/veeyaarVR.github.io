f6ea1883c89d4aa0a6b1d172359048a1ee2b4d2f
vignesh <vignesh.m@cumulations.com>
2021-08-30 21:35:50 +0530

    modified structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,52 @@
-import React , {useEffect}from "react";
-import { BrowserRouter as Router, Switch, Route, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "../screens/Home";
 import Personal from "../screens/Personal";
 import Professional from "../screens/Professional";
 import Blog from "../screens/Blog";
 import Book from "../screens/Book";
-import "../styles/styles.scss"
-import ReactGA from 'react-ga';
+import "../styles/styles.scss";
+import ReactGA from "react-ga";
 import InitializeReactGA from "../helpers/googleAnalytics";
 
 function usePageViews() {
-	let location = useLocation();
-	useEffect(() => {
-		InitializeReactGA(ReactGA);
-		ReactGA.set({ page: location.pathname });
-		ReactGA.pageview(location.pathname);
-	}, [location]);
+  let location = useLocation();
+  useEffect(() => {
+    InitializeReactGA(ReactGA);
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, [location]);
 }
 
 export default function App() {
   usePageViews();
   return (
-    <Router>
-        <div className="home">
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route exact path="/personal">
-              <Personal />
-            </Route>
-            <Route exact path="/professional">
-              <Professional />
-            </Route>
-            <Route exact path="/personal/blog">
-              <Blog />
-            </Route>
-            <Route exact path="/personal/blog/:id">
-              <Book />
-            </Route>
-            <Route exact path="/">
-            </Route>
-          </Switch>
-        </div>
-    </Router>
+    <main>
+      <div className="home">
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route exact path="/personal">
+            <Personal />
+          </Route>
+          <Route exact path="/professional">
+            <Professional />
+          </Route>
+          <Route exact path="/personal/blog">
+            <Blog />
+          </Route>
+          <Route exact path="/personal/blog/:id">
+            <Book />
+          </Route>
+          <Route exact path="/"></Route>
+        </Switch>
+      </div>
+    </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router} from "react-router-dom";
 
 const client = new ApolloClient({
   uri: `${process.env.REACT_APP_BACKEND_URL}`,
   cache: new InMemoryCache(),
-}); 
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
