0d140e710b49a1a28db77c0949908a335172ca5b
veeyaarVR <vigneshmarimuthu2302@gmail.com>
2024-07-11 09:36:33 +0530

    fix: journal page not loading issue

--- src/pages/journal.tsx
+++ src/pages/journal.tsx
@@ -2,46 +2,47 @@
 
 import React, { useEffect, useState } from "react";
 import { account } from "../api/appwrite";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 const JournalScreen = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [user, setUser] = useState(null);
 
+
+
   useEffect(() => {
-    async () => { 
+    const getUser = async () => {
       try {
         const accountData = await account.get();
         console.log(accountData);
-        if (!accountData['labels'].includes("admin")) {
-            router.push('/authfailed')
+        if (!accountData["labels"].includes("admin")) {
+          router.push("/authfailed");
         } else {
           setUser(accountData);
         }
       } catch {
         // redirect to login screen, if user is not logged in yet
-        router.push('/login')
+        router.push("/login");
       }
-    }
+    };
+    getUser();
   }, [router]);
 
   if (user == null) {
-    return <div>validating...</div>    
+    return <div>validating...</div>;
   } else {
     return (
-        <div className="flex flex-col justify-center items-center p-5">
-          <h1 className="text-3xl">Dashboard</h1>
-          {user && (
-            <div className="mt-3">
-              <p>Username:{user['name']} </p>
-              <p>Email: {user['email']}</p>
-            </div>
-          )}
-        </div>
-      );
+      <div className="flex flex-col justify-center items-center p-5">
+        <h1 className="text-3xl">Dashboard</h1>
+        {user && (
+          <div className="mt-3">
+            <p>Username:{user["name"]} </p>
+            <p>Email: {user["email"]}</p>
+          </div>
+        )}
+      </div>
+    );
   }
-
-
 };
 
 export default JournalScreen;
